a329d65d8cd7c8f247fc77297308b9552af0343c
kimsansu <bta3405@naver.com>
2023-03-05 23:05:29 +0900

    js prac

---
+++ 연습장/자바스크립트 연습/app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "Sansu";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "KimSansu";
+
+console.log("Your New Name is " + myName);
